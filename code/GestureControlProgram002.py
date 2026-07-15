62b2f2e68ad1a951dcc5c579ed9b0808ae7fa301
NikolayTkachenko <tkachenni@mail.ru>
2026-07-16 00:14:46 +0300

    update project

--- code/GestureControlProgram002.py
+++ code/GestureControlProgram002.py
@@ -5,12 +5,10 @@ import cv2
 import mediapipe as mp
 import time
 import numpy as np
-from _ctypes import POINTER
+from ctypes import POINTER
 from comtypes import CLSCTX_ALL
-from pycaw.api.endpointvolume import IAudioEndpointVolume
-
 import HandTrackingModule as htm
-from pycaw.pycaw import AudioUtilities
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 wCam, hCam = 1280, 720
 cap = cv2.VideoCapture(0)
@@ -21,22 +19,21 @@ pTime = 0
 cTime = 0
 
 detector = htm.handDetector(detectionCon=0.7)
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume._iid_, CLSCTX_ALL, None
-)
-volume = cast(interface, POINTER(IAudioEndpointVolume))
-print(volume.GetVolumeRange())
 
+# Alternative explicit method
+devices = AudioUtilities.GetDeviceEnumerator()
+speakers = devices.GetDefaultAudioEndpoint(0, 1) # 0 = eRender, 1 = eMultimedia
+interface = speakers.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = interface.QueryInterface(IAudioEndpointVolume)
 
+volRange = volume.GetVolumeRange()
+print(volRange)
 
-# device = AudioUtilities.GetSpeakers()
-# volume = device.EndpointVolume
-# print(f"Audio output: {device.FriendlyName}")
-# print(f"- Muted: {bool(volume.GetMute())}")
-# print(f"- Volume level: {volume.GetMasterVolumeLevel()} dB")
-# print(f"- Volume range: {volume.GetVolumeRange()[0]} dB - {volume.GetVolumeRange()[1]} dB")
-# volume.SetMasterVolumeLevel(0, None)
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
+volPer = 0
 
 while True:
     success, img = cap.read()
@@ -61,8 +58,24 @@ while True:
         length = math.hypot(x2-x1, y2-y1)
         print(length)
 
+        # Hand range 50 - 300
+        # Volume Range -65 -0
+
+        vol = np.interp(length, [50, 300], [minVol, maxVol])
+        volBar = np.interp(length, [50, 300], [400, 150])
+        volPer = np.interp(length, [50, 300], [0, 100])
+
+        print(int(length), vol)
+
+        volume.SetMasterVolumeLevel(vol, None)
+
         if length<50:
             cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+
+        # cv2.rectangle(img, (50, 150), (85, 400), (2, 255, 0), cv2.FILLED)
+    cv2.rectangle(img, (50, 150), (85, 400), (2, 255, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_PLAIN, 3, (0, 250, 0), 3)
 
 
     cTime = time.time()
