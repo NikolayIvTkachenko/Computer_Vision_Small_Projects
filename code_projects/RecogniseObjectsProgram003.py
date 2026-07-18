f55739ac5fdbc1aeb0b233e1c87ebbe0c0e749a1
NikolayTkachenko <tkachenni@mail.ru>
2026-07-18 03:43:47 +0300

    update project +recognise auto number

--- code_projects/RecogniseObjectsProgram003.py
+++ code_projects/RecogniseObjectsProgram003.py
@@ -10,6 +10,8 @@ cv2.namedWindow("track")
 cv2.createTrackbar("T1", "track", 0, 255, nothing)
 cv2.createTrackbar("T2", "track", 0, 255, nothing)
 
+kernel=np.ones((5, 5))
+
 while True:
     ret, frame = cap.read()
 
@@ -22,9 +24,18 @@ while True:
     thresh2=cv2.getTrackbarPos("T2", "track")
 
     canny=cv2.Canny(gray, threshold1=thresh1, threshold2=thresh2)
+    dil=cv2.dilate(canny, kernel, iterations=1)
+
+    contours, h = cv2.findContours(dil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > 10000: # Более менее оптимальное число
+            cv2.drawContours(frame, contour, -1, (200, 200, 0), 3)
+
 
     cv2.imshow("frame", frame)
-    cv2.imshow("Gray", gray)
+    # cv2.imshow("Gray", gray)
+    cv2.imshow("dil",dil)
     cv2.imshow("Canny", canny)
 
     k=cv2.waitKey(1) & 0xFF
