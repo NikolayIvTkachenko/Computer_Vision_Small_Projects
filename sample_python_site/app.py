2e7bd51df6757aa1fd2619259aadf18eae274ea3
NikolayTkachenko <tkachenni@mail.ru>
2026-08-06 02:00:34 +0300

    update project +recognise auto number +opencv course +simple neural network +cuda + GPU + update = Blur face sample app + sample python site

---
+++ sample_python_site/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+db = SQLAlchemy(app)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+
+@app.route("/index")
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/create", methods=['POST', 'GET'])
+def create():
+    if request.methos == 'POST':
+        print(request.form['title'])
+        print(request.form['text'])
+        title = request.form['title']
+        text = request.form['text']
+        post = Post(title=title, text=text)
+        try:
+            db.session.add(post)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Придобавлении записи произошла ошибка"
+    else:
+        return render_template('create.html')
+
+
+@app.route("/posts")
+def posts():
+    return render_template("posts.html")
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
