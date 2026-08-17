d25407d42b1f1209f01dd9c9d7cbbe0b0f1797e0
NikolayTkachenko <tkachenni@mail.ru>
2026-08-18 00:18:27 +0300

    update project +recognise auto number +opencv course +simple neural network +cuda + GPU + update = Blur face sample app + sample python site + example machine learning

--- data_science/SampleProgramApp005.py
+++ data_science/SampleProgramApp005.py
@@ -1,2 +1,98 @@
 print("БАЗЫ ДАННЫХ ==========")
 print("1 => =================")
+# SELECT * FROM orders WHERE status = 'Shipped';
+# SELECT * FROM orders;
+# SELECT pono, date FROM orders;
+# mysql -uroot
+# /usr/local/mysql/bin/mysql - uroot -p
+# ALTER USER 'root'@'localhost' IDENTIFIED BY 'your_new_pswd';
+#
+# CREATE DATABASE sampledb;
+# USE sampledb;
+#
+# CREATE TABLE emps (empno INT NOT NULL, empname VARCHAR(50), job VARCHAR(30), PRIMARY KEY (empno));
+# CREATE TABLE salary (empno INT NOT NULL, salary INT, PRIMARY KEY (empno));
+# ALTER TABLE salary ADD FOREIGN KEY (empno) REFERENCES emps (empno);
+#
+# CREATE TABLE orders (pono INT NOT NULL, empno INT NOT NULL, total INT, PRIMARY KEY (pono));
+# ALTER TABLE salary ADD FOREIGN KEY (empno) REFERENCES emps (empno);
+# CREATE TABLE orders (pono INT NOT NULL, empno INT NOT NULL, total INT, PRIMARY KEY (pono), FOREIGN KEY (empno) REFERENCES (empno))
+#
+# db_science root toor
+#
+#
+# ========================================================================================== ###
+#
+
+import mysql.connector
+
+
+# Инициализируем переменные до try, чтобы они были видны в finally
+cnx = None
+cursor = None
+
+
+try:
+    cnx = mysql.connector.connect(user='root', password='toor', host='localhost', port=3316, database='db_science') #toortoor
+    cursor = cnx.cursor()
+    emps = [
+        (9001, "Jeff Russell", "sales"),
+        (9002, "Jeff Russell", "sales"),
+        (9003, "Jeff Russell", "sales")
+    ]
+    query_add_emp = ("""INSERT INTO emps (empno, empname, job) VALUES (%s, %s, %s)""")
+
+    for emp in emps:
+        cursor.execute(query_add_emp, emp)
+    salary = [
+        (9001, 3000),
+        (9002, 2800),
+        (9003, 2500)
+    ]
+
+    query_add_salary = ("""INSERT INTO salary (empno, salary) VALUES (%s, %s)""")
+
+    for sal in salary:
+        cursor.execute(query_add_salary, sal)
+
+    orders = [
+        (2608, 9001, 35),
+        (2617, 9001, 35),
+        (2620, 9001, 139),
+        (2621, 9002, 95),
+        (2626, 9002, 218),
+    ]
+
+    query_add_order = ("""INSERT INTO orders(pono, empno, total) VALUES (%s, %s, %s)""")
+
+    for order in orders:
+        cursor.execute(query_add_order, order)
+
+    cnx.commit()
+
+
+except mysql.connector.Error as err:
+    print("Error-Code:", err.errno)
+    print("Error-Message: {}".format(err.msg))
+    # Если транзакция начала записываться, но упала на середине — откатываем
+    if cnx and cnx.is_connected():
+        cnx.rollback()
+
+    print(f"Ошибка MySQL [{err.errno}]: {err.msg}")
+finally:
+    if cursor:
+        cursor.close()
+    if cnx and cnx.is_connected():
+        cnx.close()
+        print("Соединение с БД закрыто.")
+
+#    cursor.close()
+# NameError: name
+# 'cursor' is not defined
+# ==============================================================================================
+# USE db_science;
+# CREATE TABLE emps (empno INT NOT NULL, empname VARCHAR(50), job VARCHAR(30), PRIMARY KEY (empno));
+# CREATE TABLE salary (empno INT NOT NULL, salary INT, PRIMARY KEY (empno));
+# ALTER TABLE salary ADD FOREIGN KEY (empno) REFERENCES emps (empno);
+# CREATE TABLE orders (pono INT NOT NULL, empno INT NOT NULL, total INT, PRIMARY KEY (pono));
+# ALTER TABLE salary ADD FOREIGN KEY (empno) REFERENCES emps (empno);
