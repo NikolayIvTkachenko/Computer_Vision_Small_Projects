8d22c9c88e047eb7a07b4df98634983007b75f99
NikolayTkachenko <tkachenni@mail.ru>
2026-08-25 00:11:53 +0300

    update project +recognise auto number +opencv course +simple neural network +cuda + GPU + update = Blur face sample app + sample python site + example machine learning

--- python_algorithms/AlgorithmApp0003_Euklid.py
+++ python_algorithms/AlgorithmApp0003_Euklid.py
@@ -15,3 +15,171 @@ def gcd(x, y):
     if (remainder != 0):
         return (gcd(smaller, remainder))
 
+
+# Квадрат Ло Шу
+
+loushu = [[4, 9, 2], [3, 5, 7], [8, 1, 6]]
+
+def verifysquare(square):
+    sums = []
+    rowsums = [sum(square[i]) for i in range(0, len(square))]
+    sums.append(rowsums)
+
+    colsums = [sum([row[i] for row in square]) for i in range(0, len(square))]
+    sums.append(colsums)
+
+    maindiag = sum([square[i][i] for i in range(0, len(square))])
+    sums.append([maindiag])
+
+    antidiag = sum([square[i][len(square) - 1 - i] for i in range(0, len(square))])
+    sums.append([antidiag])
+
+    flattened = [j for i in sums for j in i]
+
+    return(len(list(set(flattened))) == 1)
+
+print(verifysquare(loushu))
+
+# Реализация алгоритма Курусимы
+#            |       n^2      |
+# --------------------------------------------              ---------------------------
+#      n     |    (n^2+1)/2   | n^2 + 1 - n
+# --------------------------------------------              ---------------------------
+#            |        1       |
+
+
+n = 7
+square = [[float('nan') for i in range(0, n)] for j in range(0,n)]
+print(square)
+
+
+def printsquare(square):
+    labels = ['[' + str(x) + ']' for x in range(0, len(square))]
+    format_row = "{:>6}" * (len(labels) + 1)
+    print(format_row.format("", *labels))
+    for label, row in zip(labels,square):
+        print(format_row.format(label, *row))
+
+import math
+center_i = math.floor(n/2)
+center_j = math.floor(n/2)
+
+print(center_i)
+print(center_j)
+
+square[center_i][center_j] = int((n**2+1) / 2)
+square[center_i + 1][center_j] = 1
+square[center_i - 1][center_j] = n**2
+square[center_i][center_j + 1] = n**2 + 1 - n
+square[center_i][center_j - 1] = n
+
+def rule1(x, n):
+    return ((x + n) % n**2)
+
+def rule1(x, n, upright):
+    return((x + ((-1) ** upright) * n) % n**2)
+
+def rule2(x, n, upleft):
+    return((x + ((-1)**upleft)) % n **2)
+
+def rule3(x, n, upleft):
+    return((x + ((-1) ** upleft * (-n + 1))) % n**2)
+
+
+print(rule1(5,3))
+
+center_i = math.floor(n/2)
+center_j = math.floor(n/2)
+
+import random
+entry_i = center_i
+entry_j = center_j
+
+where_we_can_go = ['up_left', 'up_right', 'down_left', 'down_right']
+where_to_go = random.choice(where_we_can_go)
+
+if(where_to_go == 'up_right'):
+    new_entry_i = entry_i - 1
+    new_entry_j = entry_j + 1
+    square[new_entry_i][new_entry_j] = rule1(square[entry_i][entry_j], n, True)
+
+if(where_to_go == 'down_left'):
+    new_entry_i = entry_i + 1
+    new_entry_j = entry_j + 1
+    square[new_entry_i][new_entry_j] = rule1(square[entry_i][entry_j], n, False)
+
+if(where_to_go == 'up_left'):
+    new_entry_i = entry_i - 1
+    new_entry_j = entry_j - 1
+    square[new_entry_i][new_entry_j] = rule2(square[entry_i][entry_j], n, True)
+
+if(where_to_go == 'down_right'):
+    new_entry_i = entry_i + 1
+    new_entry_j = entry_j + 1
+    square[new_entry_i][new_entry_j] = rule2(square[entry_i][entry_j], n, False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
