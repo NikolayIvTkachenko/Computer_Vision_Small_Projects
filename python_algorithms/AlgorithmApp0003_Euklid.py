55c2acb3467e3d0515663d7be068537d6bba1a2b
NikolayTkachenko <tkachenni@mail.ru>
2026-08-25 02:47:08 +0300

    update project +recognise auto number +opencv course +simple neural network +cuda + GPU + update = Blur face sample app + sample python site + example machine learning

--- python_algorithms/AlgorithmApp0003_Euklid.py
+++ python_algorithms/AlgorithmApp0003_Euklid.py
@@ -80,13 +80,13 @@ def rule1(x, n, upright):
     return((x + ((-1) ** upright) * n) % n**2)
 
 def rule2(x, n, upleft):
-    return((x + ((-1)**upleft)) % n **2)
+    return((x + ((-1)**upleft)) % n**2)
 
 def rule3(x, n, upleft):
-    return((x + ((-1) ** upleft * (-n + 1))) % n**2)
+    return((x + ((-1)**upleft * (-n + 1)))%n**2)
 
 
-print(rule1(5,3))
+# print(rule1(5,3))
 
 center_i = math.floor(n/2)
 center_j = math.floor(n/2)
@@ -117,6 +117,130 @@ if(where_to_go == 'down_right'):
     new_entry_i = entry_i + 1
     new_entry_j = entry_j + 1
     square[new_entry_i][new_entry_j] = rule2(square[entry_i][entry_j], n, False)
+
+
+if(where_to_go == 'up_left' and (entry_i + entry_j) == (n)):
+    new_entry_i = entry_i - 1
+    new_entry_j = entry_j - 1
+    square[new_entry_i][new_entry_j] = rule3(square[entry_i][entry_j], n ,True)
+
+if(where_to_go == 'down_right' and (entry_i + entry_j) == (n - 2)):
+    new_entry_i = entry_i + 1
+    new_entry_j = entry_j + 1
+    square[new_entry_i][new_entry_j] = rule3(square[entry_i][entry_j], n, False)
+
+where_we_can_go = []
+
+if(entry_i < (n - 1) and entry_j < (n - 1)):
+    where_we_can_go.append('down_right')
+
+if(entry_i < (n - 1) and entry_j > 0):
+    where_we_can_go.append('down_left')
+
+if(entry_i > 0 and entry_j < (n - 1)):
+    where_we_can_go.append('up_right')
+
+if(entry_i > 0 and entry_j > 0):
+    where_we_can_go.append('up_left')
+
+import random
+
+def fillsquare(square, entry_i, entry_j, howfull):
+    while(sum(math.isnan(i) for row in square for i in row) > howfull):
+        where_we_can_go = []
+        if (entry_i < (n - 1) and entry_j < (n - 1)):
+            where_we_can_go.append('down_right')
+        if (entry_i < (n - 1) and entry_j > 0):
+            where_we_can_go.append('down_left')
+        if (entry_i > 0 and entry_j < (n - 1)):
+            where_we_can_go.append('up_right')
+        if (entry_i > 0 and entry_j > 0):
+            where_we_can_go.append('up_left')
+
+        where_to_go = random.choice(where_we_can_go)
+
+        if (where_to_go == 'up_right'):
+            new_entry_i = entry_i - 1
+            new_entry_j = entry_j + 1
+            square[new_entry_i][new_entry_j] = rule1(square[entry_i][entry_j], n, True)
+
+        if (where_to_go == 'down_left'):
+            new_entry_i = entry_i + 1
+            new_entry_j = entry_j - 1
+            square[new_entry_i][new_entry_j] = rule1(square[entry_i][entry_j], n, False)
+
+        if(where_to_go == 'up_left' and (entry_i + entry_j) != (n)):
+            new_entry_i = entry_i - 1
+            new_entry_j = entry_j - 1
+            square[new_entry_i][new_entry_j] = rule2(square[entry_i][entry_j], n, True)
+
+        if(where_to_go == 'down_right' and (entry_i + entry_j) != (n - 2)):
+            new_entry_i = entry_i + 1
+            new_entry_j = entry_j + 1
+            square[new_entry_i][new_entry_j] = rule2(square[entry_i][entry_j], n, False)
+
+        if(where_to_go == 'up_left' and (entry_i + entry_j) == (n)):
+            new_entry_i = entry_i - 1
+            new_entry_j = entry_j - 1
+            square[new_entry_i][new_entry_j] = rule3(square[entry_i][entry_j], n, True)
+
+        if(where_to_go == 'down_right' and (entry_i + entry_j) == (n - 2)):
+            new_entry_i = entry_i + 1
+            new_entry_j = entry_j + 1
+            square[new_entry_i][new_entry_j] = rule3(square[entry_i][entry_j], n, False)
+        entry_i = new_entry_i
+        entry_j = new_entry_j
+
+    return square
+
+print("==========================================================")
+entry_i = math.floor(n/2)
+entry_j = math.floor(n/2)
+
+square = fillsquare(square, entry_i, entry_j, (n**2) / 2 - 4)
+
+printsquare(square)
+print("==========================================================")
+
+entry_i = math.floor(n/2) + 1
+entry_j = math.floor(n/2)
+
+square = fillsquare(square, entry_i, entry_j, 0)
+printsquare(square)
+
+print("==========================================================")
+print("==========================================================")
+
+n = 11
+square = [[float('nan') for i in range(0, n)] for j in range(0, n)]
+
+center_i = math.floor(n/2)
+center_j = math.floor(n/2)
+
+square[center_i][center_j] = int((n**2 + 1)/2)
+square[center_i + 1][center_j] = 1
+square[center_i - 1][center_j] = n**2
+square[center_i][center_j + 1] = n**2 + 1 - n
+square[center_i][center_j - 1] = n
+
+entry_i = center_i
+entry_j = center_j
+
+square = fillsquare(square, entry_i, entry_j, (n**2)/2 - 4)
+
+entry_i = math.floor(n/2) + 1
+entry_j = math.floor(n/2)
+
+square = fillsquare(square, entry_i, entry_j, 0)
+
+square = [[n**2 if x == 0 else x for x in row] for row in square]
+
+printsquare(square)
+print("==========================================================")
+
+
+
+
 
 
 
