63d84edbb09bd96a2acb73cf8fb9ac5982efe763
NikolayTkachenko <tkachenni@mail.ru>
2026-08-28 09:47:45 +0300

    update project +recognise auto number +opencv course +simple neural network +cuda + GPU + update = Blur face sample app + sample python site + example machine learning

--- python_algorithms/AlgorithmApp0006_Math_01.py
+++ python_algorithms/AlgorithmApp0006_Math_01.py
@@ -48,3 +48,57 @@ def get_number(continued_fraction):
 print(continued_fraction(105, 33, 10))
 print(get_number([3, 5, 2]))
 
+def continued_fraction_decimal(x, error_tolerance, length_tolerance):
+    output = []
+    first_term = int(x)
+    leftover = x - int(x)
+    output.append(first_term)
+    error = leftover
+
+    while error > error_tolerance and len(output) < length_tolerance:
+        next_term = math.floor(1/leftover)
+        leftover = 1 / leftover - next_term
+        output.append(next_term)
+        error = abs(get_number(output) - x)
+
+    return (output)
+
+print(continued_fraction_decimal(1.4152135623730951, 0.00001, 100))
+
+
+# Функция вычисления квадратных корней по вавилонскому алгоритму
+
+def square_root(x, y, error_tolerance):
+    print("==> Call -> square_root")
+    print(f"x = {x}")
+    print(f"y = {y}")
+    print(f"error_tolerance = {error_tolerance}")
+    our_error = error_tolerance * 2
+    print(f"our_error = {our_error}")
+    print("=> while (our_error > error_tolerance): ")
+    print("---------------------------------------")
+    while (our_error > error_tolerance):
+        z = x / y
+        y = (y + z) / 2
+        our_error = y**2 - x
+        print(f"x = {x}")
+        print(f"y = {y}")
+        print(f"z = {z}")
+        print(f"our_error = {our_error}")
+        print("---------------------------------------")
+    return y
+print(square_root(4, 1, 0.00000000001)) # ==> 2.000000000000002
+
+print(square_root(5, 1, 0.00000000001)) # ==> 2.236067977499978
+
+# ------------------------------------------------------------------------------------- #
+
+
+
+
+
+
+
+
+
+
